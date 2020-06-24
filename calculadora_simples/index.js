1ab14ef483b469f80f11be015edc4e0175af93f4
Gabriel Silva Andrade <34127765+Gabrielsilvaa@users.noreply.github.com>
2020-06-24 14:44:03 -0300

    Add files via upload

---
+++ calculadora_simples/index.js
@@ -0,0 +1,30 @@
+// Vamos iniciar declarando os valores 
+
+/* SOMA */
+var n1 = 2;
+var n2 = 4;
+
+var soma = n1 + n2;
+/**aqui tambem ultilizamos o operador + para concatenar as variaveis do tipo string com as int */
+console.log("a soma dos numeros " + n1 + " + " + n2 + " é " + soma);
+
+/** SUBTRACAO */
+var n1 = 2;
+var n2 = 4;
+
+var subtracao = n1 - n2;
+console.log(subtracao);
+
+/** MULTIPLICACAO */
+var n1 = 2;
+var n2 = 4;
+
+var multiplicacao = n1 * n2;
+console.log(multiplicacao);
+
+/**DIVASAO */
+var n1 = 2;
+var n2 = 4;
+
+var divosao = n1 + n2;
+console.log(divosao);
