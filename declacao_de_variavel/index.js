d737250e7e6cb3d188ba4d438dbf8b10120a6d5b
Gabriel Silva Andrade <34127765+Gabrielsilvaa@users.noreply.github.com>
2020-06-24 14:22:00 -0300

    primeiros upload

---
+++ declacao_de_variavel/index.js
@@ -0,0 +1,16 @@
+// variavel do tipo string 
+var aluno = "Gabriel";
+
+//variavel do tipo objeto
+aluno = {
+    nome: "gabriel",
+    idade: 18,
+    curso: "progamador"
+}
+
+// variavel do tipo number
+var numero = 20;
+var valor = 20.10;
+
+//variavel do tipo array
+var valores = [4, 52.2, 55, 689];
